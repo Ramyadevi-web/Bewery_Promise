494f1154669a79aeba4b30aec7c757eaa5833825
Ramyadevi - web <ramyadevim7@gmail.com>
2024-12-16 16:51:39 -0800

    Card added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let url = "https://api.openbrewerydb.org/v1/breweries"; // Link for API
 
 //Element created for default UI
+let container = document.createElement("div");
+container.className = "container";
 let heading = document.createElement("h1");
 heading.innerText = "Select the city to get Bewery details of particular city";
 let dropdownDiv = document.createElement("div");
@@ -8,11 +10,13 @@ dropdownDiv.className = "dropdown"
 
 //Div to display the details of selected city
 let outputContainer = document.createElement("div");
-outputContainer.className = "container";
+outputContainer.className = "outputContainer";
 
-document.body.append(heading)
-document.body.append(dropdownDiv);
-document.body.append(outputContainer);
+container.appendChild(heading)
+container.appendChild(dropdownDiv);
+document.body.appendChild(container);
+document.body.appendChild(outputContainer);
+
 
 // Promise Instance created.
 let p1 = new Promise((resolve, reject) => {
@@ -31,11 +35,19 @@ let p1 = new Promise((resolve, reject) => {
 });
 
 // Function to display the Bewery details of selected city.
-function displayDetails(city,name,type) {
+function displayDetails(city,name,type,website_url) {
   outputContainer.style.display = "block";
-  outputContainer.innerHTML = `<div>${city}</div>
-  <div><span class="subHeading">Bewery Name :</span> ${name}</div>
-  <div><span class="subHeading">Bewery-Type :</span> ${type}</div>`
+  const beweryDetailsHTML = `<div class="card text-center">
+  <div class="card-header">
+  ${city}
+  </div>
+  <div class="card-body">
+    <p class="card-text"><span>Bewery Name:</span>  ${name}</p>
+    <p class="card-text"><span>Bewery Type:</span> ${type}</p>
+    <a href="${website_url}" class="btn btn-primary">Visit the website</a>
+  </div>
+</div>`
+outputContainer.innerHTML = beweryDetailsHTML;
 }
 
 
@@ -48,7 +60,8 @@ p1.then((data) => {
         </button>
         <ul class="dropdown-menu" id="city-list">
          ${data.map((ele)=>
-        `<li data-city="${ele.city}" data-name="${ele.name}" data-breweryType="${ele.brewery_type}">${ele.city}</li>`
+        `<li data-city="${ele.city}" data-name="${ele.name}" data-breweryType="${ele.brewery_type}" 
+        data-link="${ele.website_url}">${ele.city}</li>`
       ).join('')}
         </ul>
     </div>`;
@@ -60,9 +73,9 @@ p1.then((data) => {
     let city = item.getAttribute("data-city");
     let name = item.getAttribute("data-name");
     let type = item.getAttribute("data-breweryType");
-
+    let website_url= item.getAttribute("data-link")
     //Call the function to display output if action happen.
-    displayDetails(city,name,type);
+    displayDetails(city,name,type,website_url);
   })
 })
 
